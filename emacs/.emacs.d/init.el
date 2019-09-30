74057cc1b8521afe12b3fcb85e2ab2dfa92dc4c1
jackiebin <536408205@qq.com>
2019-09-30 15:06:47 +0800

    emacs update packages commands

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -14,10 +14,10 @@
  '(initial-major-mode (quote emacs-lisp-mode))
  '(package-selected-packages
    (quote
-    (pyim py-autopep8 elpy rg doom-modeline all-the-icons-dired ivy-hydra rjsx-mode js2-mode expand-region smartparens counsel-projectile counsel cider command-log-mode racer rust-mode race flycheck-rust cargo irony irony-eldoc flycheck-irony cyberpunk-theme atomic-chrome anzu undo-tree highlight-parentheses all-the-icons rainbow-mode flx smex helpful dockerfile-mode yasnippet tide web-mode markdown-mode exec-path-from-shell which-key use-package popup paredit highlight flyspell-popup magit magit-popup ghub diff-hl company platformio-mode highlight-numbers parent-mode proceed editorconfig diminish scss-mode))))
+    (paradox pyim py-autopep8 elpy rg doom-modeline all-the-icons-dired ivy-hydra rjsx-mode js2-mode expand-region smartparens counsel-projectile counsel cider command-log-mode racer rust-mode race flycheck-rust cargo irony irony-eldoc flycheck-irony cyberpunk-theme atomic-chrome anzu undo-tree highlight-parentheses all-the-icons rainbow-mode flx smex helpful dockerfile-mode yasnippet tide web-mode markdown-mode exec-path-from-shell which-key use-package popup paredit highlight flyspell-popup magit magit-popup ghub diff-hl company platformio-mode highlight-numbers parent-mode proceed editorconfig diminish scss-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreguound :height 3.0 :foreground "deep sky blue")))))
