19438fb09ef483512518b7b37353ba9012aeeb66
jackiebin <536408205@qq.com>
2019-11-25 14:01:22 +0800

    edit readme

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 # dotfiles
 This is my dot files.
-
+I use [stow](https://www.gnu.org/software/stow/) to manage my dotfiles
+```
+git clone https://github.com/jackiebin/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+stow zsh # ...and whatever else you want
+```
 ## zsh
 - oh-my-zsh
 
 ## vim
-- vim-plug
+- [vim-plug](https://github.com/junegunn/vim-plug) - to manage other plugins
 
 ## Emacs
 1. Packages
 - Hydra
 - flycheck: Syntax checking
 - flyspell: Spell checking
+    - sudo pacman -S aspell
 - langtool: English checking
+    - sudo pacman -S languagetool
 - yatemplate: filetype template
-> M-x auto-insert
+    - M-x auto-insert
 - Ivy/swipper/counsel/company/helm
 2. C++
 - lsp
-- ccls: sudo pacman -S ccls
-- ggtags: GNU global and ggtags
+- ccls:
+    - sudo pacman -S ccls
+- ggtags:
+    - sudo pacman -S global
 3. Markdown: C-c C-s keywords
-- sudo pacman -S pandoc: C-c C-c keywords
+- sudo pacman -S pandoc:
+    - C-c C-c keywords
---
+++ emacs/.emacs.d/third_parties/templates/10:.*.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+AUTHOR
+
+    `user-full-name` <`user-mail-address`>
+
+DESCRIPTION
+
+LICENSE
+    This script is in the public domain, free from copyrights or restrictions.
+    Created: `(format-time-string "%e %B %Y")`
+"""
+
+# System/default
+import sys
+import os
+
+# Arguments
+import argparse
+
+# Messaging/logging
+import traceback
+import time
+import logging
+
+###############################################################################
+# global constants
+###############################################################################
+LEVEL = [logging.WARNING, logging.INFO, logging.DEBUG]
+
+###############################################################################
+# Functions
+###############################################################################
+
+
+###############################################################################
+# Main function
+###############################################################################
+def main():
+    """Main entry function
+    """
+    global args
+
+
+###############################################################################
+#  Envelopping
+###############################################################################
+if __name__ == '__main__':
+    try:
+        parser = argparse.ArgumentParser(description="")
+
+        # Add options
+        parser.add_argument("-l", "--log_file", default=None,
+                            help="Logger file")
+        parser.add_argument("-v", "--verbosity", action="count", default=0,
+                            help="increase output verbosity")
+
+        # Add arguments
+        # Example : parser.add_argument("echo", help="description")
+        # TODO
+
+        # Parsing arguments
+        args = parser.parse_args()
+
+        # create logger and formatter
+        logger = logging.getLogger()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # Verbose level => logging level
+        log_level = args.verbosity
+        if (args.verbosity >= len(LEVEL)):
+            log_level = len(LEVEL) - 1
+            logger.setLevel(log_level)
+            logging.warning("verbosity level is too high, I'm gonna assume you're taking the highest (%d)" % log_level)
+        else:
+            logger.setLevel(LEVEL[log_level])
+
+        # create console handler
+        ch = logging.StreamHandler()
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+        # create file handler
+        if args.log_file is not None:
+            fh = logging.FileHandler(args.log_file)
+            logger.addHandler(fh)
+
+        # Debug time
+        start_time = time.time()
+        logger.info("start time = " + time.asctime())
+
+        # Running main function <=> run application
+        main()
+
+        # Debug time
+        logging.info("end time = " + time.asctime())
+        logging.info('TOTAL TIME IN MINUTES: %02.2f' %
+                     ((time.time() - start_time) / 60.0))
+
+        # Exit program
+        sys.exit(0)
+    except KeyboardInterrupt as e:  # Ctrl-C
+        raise e
+    except SystemExit:  # sys.exit()
+        pass
+    except Exception as e:
+        logging.error('ERROR, UNEXPECTED EXCEPTION')
+        logging.error(str(e))
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(-1)
